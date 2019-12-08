8ce1a22cb152baa65f5f6adf06ce1122463bb7fd
Saquibul Hasan Razib <saquibulhassan@gmail.com>
2019-12-08 19:38:38 +0600

    Vue form added

---
+++ src/App.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>Add Blog Post</h1>
+
+                <form @submit.prevent="isSubmitted = true">
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" class="form-control" id="title" v-model.trim="post.title">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="content">Content</label>
+                        <textarea class="form-control" id="content" v-model.lazy.trim="post.content" cols="30" rows="10"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="checkbox" id="publish-immediately" v-model="post.publishImmediately">
+                        <label for="publish-immediately">Publish immediately</label>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Share on</label>
+
+                        <div v-for="media in formData.socialMedia">
+                            <input type="checkbox" :id="media" :value="media" v-model="post.shareOn">
+                            <label :for="media">{{ media }}</label>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Category</label>
+
+                        <div v-for="category in formData.categories">
+                            <input type="radio" :id="category" :value="category" v-model="post.category">
+                            <label :for="category">{{ category }}</label>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="select-series">Series</label>
+
+                        <select class="form-control" id="select-series" v-model="post.series">
+                            <option value="">Choose Series</option>
+                            <option v-for="series in formData.series" :value="series">{{ series }}</option>
+                        </select>
+                    </div>
+
+                    <input type="submit" class="btn btn-primary" value="Publish">
+                </form>
+
+                <hr>
+
+                <table class="table table-striped" v-if="isSubmitted">
+                    <thead>
+                        <tr>
+                            <td class="col-xs-6"><strong>Field</strong></td>
+                            <td class="col-xs-6"><strong>Value</strong></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Title</td>
+                            <td>{{ post.title }}</td>
+                        </tr>
+                        <tr>
+                            <td>Content</td>
+                            <td style="white-space: pre;">{{ post.content }}</td>
+                        </tr>
+                        <tr>
+                            <td>Publish immediately</td>
+                            <td>{{ post.publishImmediately }}</td>
+                        </tr>
+                        <tr>
+                            <td>Share on</td>
+                            <td>
+                                <ul>
+                                    <li v-for="media in post.shareOn">{{ media }}</li>
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Category</td>
+                            <td>{{ post.category }}</td>
+                        </tr>
+                        <tr>
+                            <td>Series</td>
+                            <td>{{ post.series }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                isSubmitted: false,
+                post: {
+                    title: '',
+                    content: '',
+                    publishImmediately: true,
+                    shareOn: ['Facebook'],
+                    category: 'Backend',
+                    series: 'Value 1'
+                },
+                formData: {
+                    socialMedia: ['Facebook', 'Twitter', 'Google Plus', 'Linked In', 'IMO'],
+                    categories: ['Frontend', 'Backend'],
+                    series: [
+                        'Value 1',
+                        'Value 2',
+                        'Value 3',
+                        'Value 4',
+                        'Value 5',
+                        'Value 6',
+                    ]
+                }
+            };
+        }
+    }
+</script>
+
+<style>
+    input[type="radio"] + label,
+    input[type="checkbox"] + label {
+        font-weight: normal;
+    }
+</style>
